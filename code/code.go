c1216c1cd27caba19e85a5963fc0fabffc50db29
Nate Duncan <60360613+ntduncan@users.noreply.github.com>
2025-04-27 21:48:19 -0600

    catch up

--- code/code.go
+++ code/code.go
@@ -33,6 +33,7 @@ const (
 	OpSetGlobal
 	OpGetGlobal
 	OpArray
+	OpHash
 )
 
 var definitions = map[Opcode]*Definition{
@@ -55,6 +56,7 @@ var definitions = map[Opcode]*Definition{
 	OpGetGlobal:     {"OpGetGlobal", []int{2}},
 	OpSetGlobal:     {"OpSetGlobal", []int{2}},
 	OpArray:         {"OpArray", []int{2}},
+	OpHash:          {"OpHash", []int{2}},
 }
 
 func (ins Instructions) String() string {
--- vm/vm.go
+++ vm/vm.go
@@ -2,7 +2,6 @@ package vm
 
 import (
 	"fmt"
-
 	"ntduncan.com/go-compiler/code"
 	"ntduncan.com/go-compiler/compiler"
 	"ntduncan.com/go-compiler/object"
@@ -84,14 +83,17 @@ func (vm *VM) Run() error {
 			if err != nil {
 				return err
 			}
+
 		case code.OpMinus:
 			err := vm.executeMinusOperator()
 			if err != nil {
 				return err
 			}
+
 		case code.OpJump:
 			pos := int(code.ReadUint16(vm.instructions[ip+1:]))
 			ip = pos - 1
+
 		case code.OpJumpNotTruthy:
 			pos := int(code.ReadUint16(vm.instructions[ip+1:]))
 			ip += 2
@@ -100,16 +102,19 @@ func (vm *VM) Run() error {
 			if !isTruthy(condition) {
 				ip = pos - 1
 			}
+
 		case code.OpNull:
 			err := vm.push(Null)
 			if err != nil {
 				return err
 			}
+
 		case code.OpSetGlobal:
 			globalIndex := code.ReadUint16(vm.instructions[ip+1:])
 			ip += 2
 
 			vm.globals[globalIndex] = vm.pop()
+
 		case code.OpGetGlobal:
 			globalIndex := code.ReadUint16(vm.instructions[ip+1:])
 			ip += 2
@@ -118,6 +123,7 @@ func (vm *VM) Run() error {
 			if err != nil {
 				return err
 			}
+
 		case code.OpArray:
 			numElements := int(code.ReadUint16(vm.instructions[ip+1:]))
 			ip += 2
@@ -126,6 +132,21 @@ func (vm *VM) Run() error {
 			vm.sp = vm.sp - numElements
 
 			err := vm.push(array)
+			if err != nil {
+				return err
+			}
+
+		case code.OpHash:
+			numElements := int(code.ReadUint16(vm.instructions[ip+1:]))
+			ip += 2
+
+			hash, err := vm.buildHash(vm.sp-numElements, vm.sp)
+			if err != nil {
+				return err
+			}
+			vm.sp = vm.sp - numElements
+
+			err = vm.push(hash)
 			if err != nil {
 				return err
 			}
@@ -310,4 +331,24 @@ func (vm *VM) buildArray(startIndex, endIndex int) object.Object {
 	}
 
 	return &object.Array{Elements: elements}
+}
+
+func (vm *VM) buildHash(startIndex, endIndex int) (object.Object, error) {
+	hashedPairs := make(map[object.HashKey]object.HashPair)
+
+	for i := startIndex; i < endIndex; i += 2 {
+		key := vm.stack[i]
+		value := vm.stack[i+1]
+
+		pair := object.HashPair{Key: key, Value: value}
+
+		hashKey, ok := key.(object.Hashable)
+		if !ok {
+			return nil, fmt.Errorf("unusable as hash key: %s", key.Type())
+		}
+
+		hashedPairs[hashKey.HashKey()] = pair
+	}
+
+	return &object.Hash{Pairs: hashedPairs}, nil
 }
