f4a99e36ce5102b22d42291a35b2bca60a664bb9
Nate Duncan <60360613+ntduncan@users.noreply.github.com>
2025-04-28 21:35:07 -0600

    Finished String, Array, and Hash functionality

--- code/code.go
+++ code/code.go
@@ -34,6 +34,7 @@ const (
 	OpGetGlobal
 	OpArray
 	OpHash
+	OpIndex
 )
 
 var definitions = map[Opcode]*Definition{
@@ -57,6 +58,7 @@ var definitions = map[Opcode]*Definition{
 	OpSetGlobal:     {"OpSetGlobal", []int{2}},
 	OpArray:         {"OpArray", []int{2}},
 	OpHash:          {"OpHash", []int{2}},
+	OpIndex:         {"OpIndex", []int{}},
 }
 
 func (ins Instructions) String() string {
